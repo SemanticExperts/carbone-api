54f55c8e57ddcd787f23e757671f1d9421038ef8
Christophe <christophe@desclaux.me>
2022-01-31 14:26:38 +0100

    increase multer limit

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const express = require('express'); // REST API
 const carbone = require('carbone'); // CarboneJS
 const fs = require('fs'); // File system
 const multer  = require('multer'); // Possibility to retrieve files from multi-part request
-const upload = multer({ dest: 'templates/' });
+const upload = multer(
+    { dest: 'templates/' },
+    limits: { fieldSize: 2 * 1024 * 1024 }
+);
 
 // Constants
 const PORT = 8083;
@@ -30,24 +33,35 @@ app.post('/parseFile', requestComponents, function (req, res, next) {
     const _template = req.files['template'][0];
     const _content = JSON.parse(req.body['content']);
     const _outputFormat = req.body['outputFormat'];
-    const _formatOptions = req.body['formatOptions'];
+    let _formatOptions = req.body['formatOptions'];
     const _fileName = req.body['fileName'];
 
-    console.log("Received a new transformation Request for fileName=" + _fileName);
 
+console.log(_formatOptions);
+
+    if(_formatOptions) {
+        _formatOptions = JSON.parse(_formatOptions);
+    }
+
+    console.log("Received a new transformation Request for fileName=" + _fileName);
     const _options = {
-        convertTo: _outputFormat,
-        formatOptions : _formatOptions,
+        convertTo: {
+		formatName: _outputFormat,
+        	formatOptions : _formatOptions
+	},
         reportName: _fileName,
         lang: "fr",
         timezone: "Europe/Paris"
     };
 
+    console.log("Options = ", _options);
+    console.log(JSON.stringify(_content));
     carbone.render(_template.path, _content, _options, (err, result, reportName) => {
         if (err) {
             res.status(400);
             res.send(err.message);
             console.log("    " + err.message);
+            console.log(err);
         } else {
             fs.writeFileSync('./build/' + reportName, result);
             res.setHeader('Content-disposition', 'attachment; filename=' + reportName);
